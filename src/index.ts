7ff04cba2144bc70e8cc56b844a9bf87d47da730
maxherrmann <m@xherrmann.com>
2023-10-15 00:32:58 +0300

    Clean up package exports

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,20 @@ import logger from "./logger"
 import scanner from "./scanner"
 import version from "./version"
 
-export { BluBluetooth, default as bluetooth } from "./bluetooth"
+export { default as bluetooth } from "./bluetooth"
 export {
 	default as BluCharacteristic,
 	BluCharacteristicProperties,
 } from "./characteristic"
-export {
-	BluConfiguration,
-	BluConfigurationOptions,
-	default as configuration,
-} from "./configuration"
+export { default as configuration } from "./configuration"
 export * from "./descriptions"
 export { default as BluDescriptor } from "./descriptor"
 export { default as BluDevice } from "./device"
 export * from "./errors"
-export { BluLogger, default as logger } from "./logger"
+export { default as logger } from "./logger"
 export { default as BluRequest } from "./request"
 export { default as BluResponse } from "./response"
-export { BluScanner, default as scanner } from "./scanner"
+export { default as scanner } from "./scanner"
 export { default as BluService } from "./service"
 export { default as version } from "./version"
 
@@ -42,9 +38,12 @@ export default {
 	version: version,
 }
 
-export type { BluBluetoothEvents } from "./bluetooth"
+export type { BluBluetooth, BluBluetoothEvents } from "./bluetooth"
 export type { BluCharacteristicEvents } from "./characteristic"
+export type { BluConfiguration, BluConfigurationOptions } from "./configuration"
 export type { BluDescriptorEvents } from "./descriptor"
 export type { BluDeviceEvents } from "./device"
 export type { BluEventEmitter, eventEmitter } from "./eventEmitter"
+export type { BluLogger } from "./logger"
+export type { BluScanner } from "./scanner"
 export type { BluServiceEvents } from "./service"
